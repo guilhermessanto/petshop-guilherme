88ea57957e3d4f9a5b96f6d37f63be18fb038662
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-20 11:44:08 -0300

    roteamento

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,20 @@
+import { NavLink } from "react-router-dom";
 import estilos from "./Menu.module.css";
 const Menu = () => {
   return (
     <nav>
-      <a href="/">Blog</a>
-      <a href="/produtos">Produtos</a>
-      <a href="/sobre">Sobre</a>
-      <a href="/contato">Contato</a>
+      <NavLink exact to="/" activeClassName={estilos.ativo}>
+        Blog
+      </NavLink>
+      <NavLink to="/produtos" activeClassName={estilos.ativo}>
+        Produtos
+      </NavLink>
+      <NavLink to="/sobre" activeClassName={estilos.ativo}>
+        Sobre
+      </NavLink>
+      <NavLink to="/contato" activeClassName={estilos.ativo}>
+        Contato
+      </NavLink>
     </nav>
   );
 };
