aa1f752252f05781044a8856cdb42d40906ff934
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-20 09:52:31 -0300

    lista postes

---
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -0,0 +1,46 @@
+import estilos from "./ListaPosts.module.css";
+
+const ListaPosts = () => {
+  return (
+    <div className={estilos.lista_posts}>
+      <article className={estilos.post}>
+        <h3>Título do Post...</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+          pariatur soluta aliquam iure voluptate dolorum dolorem natus at, animi
+          neque ratione maxime ipsam, quibusdam voluptates esse. Nihil molestiae
+          velit assumenda!
+        </p>
+      </article>
+      <article className={estilos.post}>
+        <h3>Título do Post...</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+          pariatur soluta aliquam iure voluptate dolorum dolorem natus at, animi
+          neque ratione maxime ipsam, quibusdam voluptates esse. Nihil molestiae
+          velit assumenda!
+        </p>
+      </article>
+      <article className={estilos.post}>
+        <h3>Título do Post...</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+          pariatur soluta aliquam iure voluptate dolorum dolorem natus at, animi
+          neque ratione maxime ipsam, quibusdam voluptates esse. Nihil molestiae
+          velit assumenda!
+        </p>
+      </article>
+      <article className={estilos.post}>
+        <h3>Título do Post...</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+          pariatur soluta aliquam iure voluptate dolorum dolorem natus at, animi
+          neque ratione maxime ipsam, quibusdam voluptates esse. Nihil molestiae
+          velit assumenda!
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default ListaPosts;
--- src/pages/Produtos/Produtos.jsx
+++ src/pages/Produtos/Produtos.jsx
@@ -4,6 +4,35 @@ const Produtos = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Conheça nossos produtos</h2>
+      <div className={estilos.produtos}>
+        <article>
+          <h3>Produto 1</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Accusantium nisi aut facere quia? Maiores doloremque sint,
+            architecto, qui laboriosam distinctio expedita fuga cupiditate,
+            dolor perspiciatis ducimus error maxime? Sequi, quas.
+          </p>
+        </article>
+        <article>
+          <h3>Produto 2</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Accusantium nisi aut facere quia? Maiores doloremque sint,
+            architecto, qui laboriosam distinctio expedita fuga cupiditate,
+            dolor perspiciatis ducimus error maxime? Sequi, quas.
+          </p>
+        </article>
+        <article>
+          <h3>Produto 3</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Accusantium nisi aut facere quia? Maiores doloremque sint,
+            architecto, qui laboriosam distinctio expedita fuga cupiditate,
+            dolor perspiciatis ducimus error maxime? Sequi, quas.
+          </p>
+        </article>
+      </div>
     </section>
   );
 };
