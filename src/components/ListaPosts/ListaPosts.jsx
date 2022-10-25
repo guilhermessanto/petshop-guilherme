e122324cf2da1f88a3c35ae10b956d45a3d4638b
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-25 09:44:19 -0300

    loading animado

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react"; //Hooks do React
+import { useState, useEffect } from "react";
 import estilos from "./ListaPosts.module.css";
 import servidorApi from "../../api/servidor-api.js";
+import LoadingDesenho from "../LoadingDesenho/LoadingDesenho";
 const ListaPosts = () => {
-  /* Iniciamos o state do componente com um array vazio, para posteriormente "prrenchê-lo" com os dados vindo da API.
-  Esta atribuição serpa feita com auxílio do setPosts. */
   const [posts, setPosts] = useState([]);
-
-  console.log(servidorApi);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getPosts() {
@@ -14,20 +12,18 @@ const ListaPosts = () => {
         const resposta = await fetch(`${servidorApi}/posts`);
         const dados = await resposta.json();
         setPosts(dados);
+        setLoading(false);
       } catch (error) {
         console.error("Deu ruim!" + error.message);
       }
     }
     getPosts();
   }, []);
-  /* Sobre o UseEffect 
-  Este hook visa permitir um maior controle sobre "efeitos colaterais" na execução do componente.
-  
-  Recebe dois parâmetros:
-  1°: função callback com o que será executado
-  2º lista de dependências que indicarão ao useEffect quando ele deverá funcionar
-  - Se não passar a lista (ou seja, se deixar sem os [], useEffect executará toda vez que i componente for renderizado. Portanto, o callback se torna um loop infinito.
-  - Se passar a lista vazia (ou seja, deixar o [] vazio), useEffect executará somente no momento que o componente é renderizado a primeira vez evitando o loop inginito do callback. */
+
+  if (loading) {
+    return <LoadingDesenho />;
+  }
+
   return (
     <div className={estilos.lista_posts}>
       {posts.map(({ id, titulo, subtitulo }) => (
