185d4e1f570dce1eb9d3a38dab1431c5c30ea8af
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-28 09:30:32 -0300

    form basico

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,3 +1,4 @@
+import { TextField, Button } from "@mui/material";
 import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 
@@ -6,7 +7,42 @@ const Contato = () => {
     <section>
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
       <Caixa>
-        <p>em breve teremos um formulario</p>
+        <form method="post">
+          <div>
+            <TextField
+              id="teste"
+              type="text"
+              label="Nome"
+              variant="outlined"
+              fullWidth
+              required
+              helperText="Informe seu nome"
+            />
+            <TextField
+              id="teste2"
+              type="email"
+              label="E-mail"
+              variant="outlined"
+              fullWidth
+              required
+              helperText="Informe seu email"
+            />
+            <TextField
+              id="test3"
+              type="text"
+              label="Mensagem"
+              multiline
+              rows={4}
+              fullWidth
+              required
+              helperText="Digite uma mensagem"
+            />
+            <div></div>
+            <Button variant="outlined" disabled>
+              ENVIAR MENSAGEM
+            </Button>
+          </div>
+        </form>
       </Caixa>
     </section>
   );
