74df178dde5a05e1a2a0c30dc04885528ec3abce
Guilherme Santana <g.santossantana22@gmail.com>
2022-10-28 11:08:40 -0300

    finalização do formulario

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,15 +1,49 @@
 import { TextField, Button } from "@mui/material";
+import { useState } from "react";
 import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
+import servidorApi from "../../api/servidor-api";
 
 const Contato = () => {
+  /* Eventos/Funções para captura da digitação */
+  const inputNome = (event) => setNome(event.target.value);
+  const inputEmail = (event) => setEmail(event.target.value);
+  const inputMensagem = (event) => setMensagem(event.target.value);
+  const enviarContato = async (event) => {
+    event.preventDefault();
+    const opcoes = {
+      method: "POST",
+      body: JSON.stringify({ nome, email, mensagem }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+    /* Script para o envio dos dados para API */
+    try {
+      await fetch(`${servidorApi}/contatos`, opcoes);
+      alert("Dados enviados!");
+    } catch (error) {
+      console.log("Deu ruim" + error.message);
+    }
+  };
+
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  let desabilitado = !nome || !email || !mensagem;
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
       <Caixa>
-        <form method="post" className={estilos.formulario}>
+        <form
+          onSubmit={enviarContato}
+          method="post"
+          className={estilos.formulario}
+        >
           <div>
             <TextField
+              onChange={inputNome}
               id="teste"
               type="text"
               label="Nome"
@@ -19,6 +53,7 @@ const Contato = () => {
               helperText="Informe seu nome"
             />
             <TextField
+              onChange={inputEmail}
               id="teste2"
               type="email"
               label="E-mail"
@@ -28,6 +63,7 @@ const Contato = () => {
               helperText="Informe seu email"
             />
             <TextField
+              onChange={inputMensagem}
               id="test3"
               type="text"
               label="Mensagem"
@@ -37,10 +73,11 @@ const Contato = () => {
               required
               helperText="Digite uma mensagem"
             />
-            <div></div>
-            <Button variant="outlined" disabled>
-              ENVIAR MENSAGEM
-            </Button>
+            <div>
+              <Button disabled={desabilitado} type="submit" variant="outlined">
+                ENVIAR MENSAGEM
+              </Button>
+            </div>
           </div>
         </form>
       </Caixa>
