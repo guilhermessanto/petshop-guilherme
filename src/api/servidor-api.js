567d547410ffed8c75cfb311e7f5c6a7ab25c72f
Guilherme Santana <g.santossantana22@gmail.com>
2022-12-02 10:26:14 -0300

    .

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,4 +1,5 @@
 const porta = "2112";
 /* const servidorApi = `http://localhost:${porta}`; */ //localhost
-const servidorApi = `http://10.20.45.23:${porta}`; //rede local
+//const servidorApi = `http://10.20.45.23:${porta}`; //rede local
+const servidorApi = `http://192.168.1.8:${porta}`;
 export default servidorApi;
